f3dc333702666d044bba05f201d77c1843867ceb
Zach Gharst <zdgharst@gmail.com>
2023-09-22 20:55:45 -0500

    Refactor game utils to own file

---
+++ src/lib/NewGameUtils.ts
@@ -0,0 +1,96 @@
+export const generateRandomNumber = (min: number, max: number): number => {
+	const range = max - min;
+	return min + Math.floor(Math.random() * range);
+};
+
+export const generateChoicesArray = (): number[] => {
+	const result = [];
+
+	let min = 1;
+	let max = 5;
+
+	for (let i = 0; i < 7; i++) {
+		result.push(generateRandomNumber(min, max));
+		min = result[result.length - 1] + 1;
+		max = min + 5;
+	}
+
+	return result;
+};
+
+export const generateGoal = (choices: number[]): number => {
+	const numberOfOperations = 4;
+	let choicesCopy = structuredClone(choices);
+	let result = 0;
+
+	const firstIndex = Math.floor(Math.random() * choicesCopy.length);
+	//const firstIndex = generateRandomNumber(0, choicesCopy.length);
+	let first = choicesCopy[firstIndex];
+	choicesCopy = choicesCopy.filter((c: number) => c !== first);
+
+	for (let i = 0; i < numberOfOperations; i++) {
+		const secondIndex = Math.floor(Math.random() * choicesCopy.length);
+		const second = choicesCopy[secondIndex];
+		choicesCopy = choicesCopy.filter((c: number) => c !== second);
+
+		const operations = ['+', '+', '-'];
+
+		if (first * second < 600) {
+			operations.push('*');
+			operations.push('*');
+			operations.push('*');
+		}
+
+		if (first % second === 0 || second % first === 0) {
+			operations.push('/');
+			operations.push('/');
+			operations.push('/');
+			operations.push('/');
+		}
+
+		const o = operations[Math.floor(Math.random() * operations.length)];
+
+		switch (o) {
+			case '+':
+				result = first + second;
+				break;
+			case '-':
+				result = first - second;
+
+				if (result < 0) {
+					result *= -1;
+				}
+
+				break;
+			case '*':
+				result = first * second;
+				break;
+			case '/':
+				if (first > second) {
+					result = first / second;
+				} else {
+					result = second / first;
+				}
+
+				break;
+		}
+
+		first = result;
+	}
+
+	if (choices.includes(result)) {
+		return generateGoal(choices);
+	}
+
+	return result;
+};
+
+export const newGame = () => {
+	const choices = generateChoicesArray();
+	const goal = generateGoal(choices);
+
+	return {
+		choices,
+		goal
+	};
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,182 +1,87 @@
 <script>
-    import { onMount } from 'svelte';
+	import { onMount } from 'svelte';
+    import { newGame } from '$lib/NewGameUtils';
 
-    let operationButtons = [ 'u', '+', '-', '*', '/' ];
-    let choices = [];
-    let goal = 0;
+	let gameState = {
+		choices: [ 0 ],
+		goal: 0,
+	};
 
-    const getRandomNumber = (min, max) => {
-        const range = max - min;
-        return min + Math.floor(Math.random() * range);
-    }
-    
-    const populateChoices = () => {
-        let result = [];
+	let operationButtons = ['u', '+', '-', '*', '/'];
 
-        let min = 1;
-        let max = 5;
-
-        for (let i = 0; i < 7; i++) {
-            result.push(getRandomNumber(min, max));
-            min = result[result.length - 1] + 1;
-            max = min + 5;
-        }
-
-        return result;
-    }
-
-    const getGoal = () => {
-        let numberOfOperations = 4;
-        let choicesCopy = structuredClone(choices);
-        let result = 0;
-
-        let firstIndex = Math.floor(Math.random() * choicesCopy.length);
-        let first = choicesCopy[firstIndex];
-        choicesCopy = choicesCopy.filter(c => c !== first);
-
-        for (let i = 0; i < numberOfOperations; i++) {
-            let secondIndex = Math.floor(Math.random() * choicesCopy.length);
-            let second = choicesCopy[secondIndex];
-            choicesCopy = choicesCopy.filter(c => c !== second);
-
-            let operations = [ '+', '+', '-' ];
-
-            if (first * second < 600) {
-                operations.push('*');
-                operations.push('*');
-                operations.push('*');
-            }
-
-            if (first % second === 0 || second % first === 0) {
-                operations.push('/');
-                operations.push('/');
-                operations.push('/');
-                operations.push('/');
-            }
-
-            let o = operations[Math.floor(Math.random() * operations.length)];
-
-            switch(o) {
-                case '+':
-                    result = first + second;
-                    break;
-                case '-':
-                    result = first - second;
-
-                    if (result < 0) {
-                        result *= -1;
-                    }
-
-                    break;
-                case '*':
-                    result = first * second;
-                    break;
-                case '/':
-                    if (first > second) {
-                        result = first / second;
-                    } else {
-                        result = second / first;
-                    }
-
-                    break;
-            }
-
-            first = result;
-        }
-
-        if (choices.includes(result)) {
-            result = getGoal();
-        }
-
-        return result;
-    }
-
-    onMount(() => {
-        choices = populateChoices();
-        goal = getGoal();
-    });
-
-    const newGame = () => {
-        choices = populateChoices();
-        goal = getGoal();
-    }
-
-    const victoryState = () => {
-        alert('You win!');
-    }
-
-    
-    if (choices.includes(goal)) {
-        victoryState();
-    }
+	onMount(() => {
+		gameState = newGame();
+	});
 </script>
 
-<h1>{goal}</h1>
+<h1>{gameState.goal}</h1>
 
 <div>
-    {#each choices as choice}
-        <button class="numberChoice">{choice}</button>
-    {/each}
+	{#each gameState.choices as choice}
+		<button class="numberChoice">{choice}</button>
+	{/each}
 </div>
 
 <div>
-    {#each operationButtons as opButton}
-        <button class="opButton">{opButton}</button>
-    {/each}
+	{#each operationButtons as opButton}
+		<button class="opButton">{opButton}</button>
+	{/each}
 </div>
 
 <div>
-    <button id="newGame" on:click={newGame}>New Game</button>
+	<button id="newGame" on:click={() => gameState = newGame()}>New Game</button>
 </div>
 
 <style>
-    h1, div {
-        animation: fadeInAnimation ease 2s;
-        animation-iteration-count: 1;
-        animation-fill-mode: forwards;
-        text-align: center;
-    }
-    
-    h1 {
-        font-size: 100px;
-        margin-top: 50px;
-    }
+	h1,
+	div {
+		animation: fadeInAnimation ease 2s;
+		animation-iteration-count: 1;
+		animation-fill-mode: forwards;
+		text-align: center;
+	}
 
-    .numberChoice, .opButton {
-        background: none;
-        border: 3px dotted #000;
-        border-radius: 38px;
-        height: 75px;
-        font-size: 24px;
-        margin: 15px;
-        width: 75px;
-    }
+	h1 {
+		font-size: 100px;
+		margin-top: 50px;
+	}
 
-    .opButton {
-        background: black;
-        color: white;
-    }
+	.numberChoice,
+	.opButton {
+		background: none;
+		border: 3px dotted #000;
+		border-radius: 38px;
+		height: 75px;
+		font-size: 24px;
+		margin: 15px;
+		width: 75px;
+	}
 
-    #newGame {
-        background: #55c2da;
-        border: 0;
-        border-radius: 5px;
-        color: white;
-        font-size: 25px;
-        font-weight: bold;
-        margin-top: 50px;
-        padding: 10px 20px;
-    }
+	.opButton {
+		background: black;
+		color: white;
+	}
 
-    @keyframes fadeInAnimation {
-        0% {
-            opacity: 0;
-        }
-        15% {
-            opacity: 0;
-        }
-        100% {
-            opacity: 1;
-        }
-    }
+	#newGame {
+		background: #55c2da;
+		border: 0;
+		border-radius: 5px;
+		color: white;
+		font-size: 25px;
+		font-weight: bold;
+		margin-top: 50px;
+		padding: 10px 20px;
+	}
+
+	@keyframes fadeInAnimation {
+		0% {
+			opacity: 0;
+		}
+		15% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
 </style>
